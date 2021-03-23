b61ac7144860c1418ab2b9b4fb70a83dec2326b6
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-03-23 19:13:16 -0300

    Finalizando app aula 4 Orientacao

--- Aula4NAOrientacao/app/src/main/java/br/com/devmob/aula4naorientacao/MainActivity.kt
+++ Aula4NAOrientacao/app/src/main/java/br/com/devmob/aula4naorientacao/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.devmob.aula4naorientacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,12 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(savedInstanceState !=null){
+            names = savedInstanceState.getStringArrayList("name_list")?: arrayListOf()
+        }
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
         lstNames.adapter = adapter
     }
 
-
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putStringArrayList("name_list", names)
+    }
     fun btnAddClick (view : View){
         names.add(edtName.text.toString())
         edtName.text.clear()
