04c8aef08d268f369dd7bce2023caa761659c8d0
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-05-11 19:10:49 -0300

    Classe Hotel criada

---
+++ Fragments/app/src/main/java/br/com/devmob/fragments/Hotel.kt
@@ -0,0 +1,10 @@
+package br.com.devmob.fragments
+
+data class Hotel(
+    var id: Long = 0,
+    var name: String = "",
+    var address: String ="",
+    var rating: Float = 0.0F
+){
+    override fun toString(): String = name
+}
