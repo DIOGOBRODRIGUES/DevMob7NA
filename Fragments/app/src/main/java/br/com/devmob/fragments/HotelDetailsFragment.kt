5c33272da3c5cb27138832a0bdb95d446088ee82
Diogo Rodrigues <diogo.borbar@gmail.com>
2021-05-11 21:50:45 -0300

    HotelDetailsFragment

--- Fragments/app/src/main/java/br/com/devmob/fragments/HotelDetailsFragment.kt
+++ Fragments/app/src/main/java/br/com/devmob/fragments/HotelDetailsFragment.kt
@@ -1,4 +1,37 @@
 package br.com.devmob.fragments
 
-class HotelDetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_hotel_details.*
+
+class HotelDetailsFragment: Fragment(), HotelDetailsView {
+    private val presenter = HotelDetailsPresenter(this, MemoryRepository)
+    private var hotel: Hotel? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hotel_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.loadHotelDetails(arguments?.getLong(EXTRA_HOTEL_ID, -1)?: -1)
+    }
+
+    override fun showHotelDetails(hotel: Hotel) {
+        this.hotel = hotel
+        txtName.text = hotel.name
+        txtAddress.text = hotel.address
+        rtbRating.rating = hotel.rating
+    }
+
+    override fun errorHotelNotFound() {
+        TODO("Not yet implemented")
+    }
 }
